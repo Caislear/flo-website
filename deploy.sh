9ea7fcd2034b28a09ba1cc3a4c96f670a64dfb60
Benjamin Woodley <bwoodley012@gmail.com>
2025-03-27 00:02:54 +0100

    chore: make deploy relative

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /Users/ben/Documents/code/ghost-static-site-generator
+cd ~/Documents/code/ghost-static-site-generator
 
 node ./src/index.js --domain "http://localhost:3001" --url "https://designsbyflow.com"
 cp -r ./static/ ~/Documents/code/flo-website/docs/
