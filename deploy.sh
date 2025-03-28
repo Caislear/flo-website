f636b94a484f2eac248c52955f3bbffcea8d5165
Benjamin Woodley <bwoodley012@gmail.com>
2025-03-28 11:49:40 +0100

    cicd: deploy via submodule

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cd ~/Documents/code/ghost-static-site-generator
+cd ./ghost-static-site-generator
+
+#ensure yarn packages are installed for gssg
+yarn
 
 node ./src/index.js --domain "http://localhost:3001" --url "https://designsbyflow.com"
-cp -r ./static/ ~/Documents/code/flo-website/docs/
-cd ~/Documents/code/flo-website
+cp -r  ./static/ ../docs
+cd ..
 git add .
 git commit -m "chore: site update $(date +%Y-%m-%d-%H:%M:%S)"
 git push
